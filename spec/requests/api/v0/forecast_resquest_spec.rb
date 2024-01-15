1bcfb6e1ccabf14c662595b3b104e47cdd66cf3e
Sam Tran <samuelttran@gmail.com>
2024-01-14 22:42:00 -0700

    Add routes for api, forecast controller, start of tests

---
+++ spec/requests/api/v0/forecast_resquest_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+describe 'Get weather for a city', type: :request do
+  describe 'GET /api/v0/forecast?location=city,st' do
+    before(:each) do
+      @location_params = { location: 'denver,co' }
+      @invalid_location_params = { location: 'denver,cooooo' }
+    end
+
+    it 'returns a successful response, 200' do
+      get '/api/v0/forecast', params: @location_params
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns forecast data for location upon successful response' do
+      get '/api/v0/forecast', params: { location: @location_params }
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data).to have_key(:data)
+      expect(data[:data]).to have_key(:id)
+      expect(data[:data]).to have_key(:type)
+      expect(data[:data]).to have_key(:attributes)
+
+      attributes = data[:data][:attributes]
+
+      expect(attributes).to have_key(:current_weather)
+      expect(attributes[:current_weather]).to have_key(:last_updated)
+      expect(attributes[:current_weather]).to have_key(:temperature)
+      expect(attributes[:current_weather]).to have_key(:feels_like)
+      expect(attributes[:current_weather]).to have_key(:humidity)
+      expect(attributes[:current_weather]).to have_key(:uvi)
+      expect(attributes[:current_weather]).to have_key(:visibility)
+      expect(attributes[:current_weather]).to have_key(:condition)
+      expect(attributes[:current_weather]).to have_key(:icon)
+
+      expect(attributes).to have_key(:daily_weather)
+      expect(attributes[:daily_weather]).to be_an(Array)
+      expect(attributes[:daily_weather].size).to eq(5)
+      attributes[:daily_weather].each do |day|
+        expect(day).to have_key(:date)
+        expect(day).to have_key(:sunrise)
+        expect(day).to have_key(:sunset)
+        expect(day).to have_key(:max_temp)
+        expect(day).to have_key(:min_temp)
+        expect(day).to have_key(:condition)
+        expect(day).to have_key(:icon)
+      end
+
+      expect(attributes).to have_key(:hourly_weather)
+      expect(attributes[:hourly_weather]).to be_an(Array)
+      expect(attributes[:hourly_weather].size).to eq(24)
+      attributes[:hourly_weather].each do |hour|
+        expect(hour).to have_key(:time)
+        expect(hour).to have_key(:temperature)
+        expect(hour).to have_key(:conditions)
+        expect(hour).to have_key(:icon)
+      end
+    end
+
+    it 'returns an unsuccessful response, 404, for an invalid location' do
+      get '/api/v0/forecast', params: invalid_location_params
+      expect(response.status).to eq(404)
+    
+      error_response = JSON.parse(response.body, symbolize_names: true)
+      expect(error_response).to have_key(:error)
+      expect(error_response[:error]).to eq('Location not found')
+    end
+  end
+end
