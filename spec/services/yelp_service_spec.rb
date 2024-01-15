3117f963d5c70ddaed831f91c07bce4cd6937395
Sam Tran <samuelttran@gmail.com>
2024-01-15 14:04:12 -0700

    Add params to #get_munchie

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -10,19 +10,19 @@ describe WeatherService, type: :service do
     it '#get_munchie, returns single top rated by location and food type' do
       location = 'pueblo,co'
       food = 'italian'
-      best_munchie = YelpService.new.best_munchie(location, food)
+      best_munchie = YelpService.new.get_munchie(location, food)
 
       expect(best_munchie).to be_a(Hash)
       expect(best_munchie).to have_key(:businesses)
       expect(best_munchie[:businesses].count).to eq(1)
 
-      restaurant = best_munchie[:businessess].first
+      restaurant = best_munchie[:businesses].first
       expect(restaurant).to have_key(:name)
       expect(restaurant[:name]).to be_a(String)
       expect(restaurant).to have_key(:location)
       expect(restaurant[:location]).to be_a(Hash)
       expect(restaurant[:location]).to have_key(:display_address)
-      expect(restaurant[:location][:display_address]).to be_a(String)
+      expect(restaurant[:location][:display_address]).to be_a(Array)
       expect(restaurant).to have_key(:review_count)
       expect(restaurant[:review_count]).to be_a(Integer)
       expect(restaurant).to have_key(:rating)
