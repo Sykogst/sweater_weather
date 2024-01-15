22910b4a5258d8c145d386c2bbec13d810fe644e
Sam Tran <samuelttran@gmail.com>
2024-01-15 10:22:43 -0700

    Test weather api service

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,8 +1,6 @@
-class WeatherFacade
+class ForecastFacade
   def weather_forecast(location, days)
     lat_lon = MapService.get_coordinates(location)
     forecast_data = WeatherService.get_forecast(lat_lon, days)
   end
-
-
 end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,15 +1,16 @@
 class WeatherService
   def conn
-    Faraday.new(url: 'http://api.weatherapi.com/v1') do |faraday|
-      faraday.params['key'] = Rails.application.credentials.weather_api
+    Faraday.new(url: 'http://api.weatherapi.com/v1/') do |faraday|
+      faraday.params['key'] = Rails.application.credentials.weather_api[:key]
     end
   end
 
   def get_forecast(lat_lon, days)
     response = conn.get do |request|
-      request.url('/forecast.json')
+      request.url('forecast.json')
       request.params['q'] = lat_lon
       request.params['days'] = days
     end
+    data = JSON.parse(response.body, symbolize_names: true)
   end
 end
