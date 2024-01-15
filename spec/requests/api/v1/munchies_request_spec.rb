923e38056eda9a01fc1fc989b35c8a8c53b7cd3b
Sam Tran <samuelttran@gmail.com>
2024-01-15 13:45:07 -0700

    Write tests for endpoint

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -5,8 +5,38 @@ describe 'Get food and forcast information for desination city', type: :request 
     it 'returns a successful response, 200, for valid params' do
       munchies_params = { destination: 'pueblo,co', food: 'italian'}
       get '/api/v1/munchies', params: munchies_params
-  
       expect(response.status).to eq(200)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data).to have_key[:data]
+      expect(data[:data]).to have_key(:id)
+      expect(data[:data][:id]).to eq("null")
+      expect(data[:data]).to have_key(:type)
+      expect(data[:data][:type]).to eq("munchie")
+      expect(data[:data]).to have_key(:attributes)
+      expect(data[:data][:id][:attributes]).to be_a(Hash)
+      expect(data[:data]).to have_key(:restaurant)
+      expect(data[:data][:id][:restaurant]).to be_a(Hash)
+
+      attributes = data[:data][:attributes]
+      expect(attributes).to have_key(:destination_city)
+      expect(attributes[:destination_city]).to be_a(String)
+      expect(attributes).to have_key(:forecast)
+      expect(attributes[:forecast]).to be_a(Hash)
+      expect(attributes[:forecast]).to have_key(:summary)
+      expect(attributes[:forecast][:summary]).to be_a(String)
+      expect(attributes[:forecast]).to have_key(:temperature)
+      expect(attributes[:forecast][:temperature]).to be_a(String)
+
+      restaurant = data[:data][:restauraunt]
+      expect(restaurant).to have_key(:name)
+      expect(restaurant[:name]).to be_a(String)
+      expect(restaurant).to have_key(:address)
+      expect(restaurant[:address]).to be_a(String)
+      expect(restaurant).to have_key(:rating)
+      expect(restaurant[:rating]).to be_a(Float).or be_a(Integer)
+      expect(restaurant).to have_key(:reviews)
+      expect(restaurant[:reviews]).to be_a(Integer)
     end
 
   end
