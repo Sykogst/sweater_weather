2452b63a045d55eb5d76caccf8677b7453edadce
Sam Tran <samuelttran@gmail.com>
2024-01-15 23:21:44 -0700

    Start building controller for RoadTripController

--- app/controllers/api/v0/road_trip_controller.rb
+++ app/controllers/api/v0/road_trip_controller.rb
@@ -1,2 +1,11 @@
 class Api::V0::RoadTripController < ApplicationController
+  def create
+    payload = JSON.parse(request.body.read, symbolize_names: true)
+    api_key = payload[:api_key]
+    origin = payload[:origin]
+    destination = payload[:destination]
+
+    road_trip_data = RoadTripFacade.new(api_key, origin, destination)
+    render json: RoadTripSerializer.new(munchie_data).to_json, status: :ok
+  end
 end
