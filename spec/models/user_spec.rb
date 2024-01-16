bc73722d42f435ef2fdfe49a30719e6321d7a469
Sam Tran <samuelttran@gmail.com>
2024-01-15 19:06:24 -0700

    Update factory and write test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'secure password and validations' do
+    it { should have_secure_password }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_confirmation_of(:password) }
+  end
+
+  describe 'generates API key' do
+    it 'generates an API key before creating a user' do
+      user = build(:user)
+      expect(user.api_key).to be_nil
+
+      user.save
+
+      expect(user.api_key).to_not be_nil
+      expect(user.api_key).to be_a(String)
+    end
+  end
 end
