4619ea0360a3c0ea4bf7b6b5a48829d06ac826c1
Sam Tran <samuelttran@gmail.com>
2024-01-16 07:20:01 -0700

    Fix method, to read through response body, so query params cannot be used

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -1,8 +1,11 @@
 class Api::V0::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
+    payload = JSON.parse(request.body.read, symbolize_names: true)
+    email = payload[:email]
+    password = payload[:password]
+    user = User.find_by(email: email)
 
-    if user && user.authenticate(params[:password])
+    if user && user.authenticate(password)
       render json: { data: { type: 'users', id: user.id.to_s, attributes: { email: user.email, api_key: user.api_key } } }, status: :ok
     else
       unauthorized_error
