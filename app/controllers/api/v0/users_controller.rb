c76f2c2868cf4ad34f3a0b0761462f026d20e0fb
Sam Tran <samuelttran@gmail.com>
2024-01-15 18:55:27 -0700

    Add users serializer, start create method

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,2 +1,20 @@
 class Api::V0::UsersController < ApplicationController
+  rescue_from ActionController::ParameterMissing, with: :param_missing_error_response
+  rescue_from ActiveRecord::RecordInvalid, with: :validation_error_response
+  
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: UserSerializer.new(user), status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity 
+    end
+  end
+
+  private 
+  
+  def user_params
+    params.requre(:user).permit(:email, :password, :password_confirmation)
+  end
 end
