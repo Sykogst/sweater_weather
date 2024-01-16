8178e769e827f68cb200b4444c18337477db4d13
Sam Tran <samuelttran@gmail.com>
2024-01-16 10:06:26 -0700

    Write facade test and expand method to handle Impossible Route

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,3 +1,20 @@
 class RoadTripFacade
+  def directions_data(origin, destination)
+    directions = MapService.new.get_directions(origin, destination)
+    locations = directions[:route][:locations]
 
+    if directions[:route].key?(:routeError)
+      directions_data = {
+        start_city: origin,
+        end_city: destination,
+        travel_time: 'Impossible Route'
+      }
+    else
+      directions_data = {
+        start_city: "#{locations.first[:adminArea5]}, #{locations.first[:adminArea3]}",
+        end_city: "#{locations.last[:adminArea5]}, #{locations.last[:adminArea3]}",
+        travel_time: directions[:route][:formattedTime]
+      }
+    end
+  end
 end
--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -2,6 +2,27 @@ require 'rails_helper'
 
 RSpec.describe RoadTripFacade, type: :facade do
   describe 'RoadTrip Facade instance methods', :vcr do
-    
+    it '#directions_data, returns hash of directions data, good search' do
+      origin = 'denver,co'
+      destination = 'boulder,co'
+      directions_data = RoadTripFacade.new.directions_data(origin, destination)
+
+      expect(directions_data).to be_a(Hash)
+      expect(directions_data[:start_city]).to eq('Denver, CO')
+      expect(directions_data[:end_city]).to eq('Boulder, CO')
+      expect(directions_data[:travel_time]).to be_a(String)
+      expect(directions_data[:travel_time]).to_not eq('Impossible Route')
+    end
+
+    it '#directions_data, returns hash of directions data, bad search' do
+      origin = 'denver,co'
+      destination = 'london'
+      directions_data = RoadTripFacade.new.directions_data(origin, destination)
+
+      expect(directions_data).to be_a(Hash)
+      expect(directions_data[:start_city]).to eq(origin)
+      expect(directions_data[:end_city]).to eq(destination)
+      expect(directions_data[:travel_time]).to eq('Impossible Route')
+    end
   end
 end
